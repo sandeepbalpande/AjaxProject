880098991b0c1c245c73763b600081d20a22d5e9
sandeepbalpande <sandipbalpande.09@gmail.com>
2020-03-31 15:02:10 +0530

    check file is added with some changes

--- just.php
+++ just.php
@@ -3,6 +3,26 @@
 $a=array(1,2,3,4,5,6,9);
 print_r($a);
 echo "Hello";
-?>
 
+
+$a=array(1,2,3,4,5,6,9);
+print_r($a);
+echo "Hello";
+
+$a=array(1,2,3,4,5,6,9);
+print_r($a);
+echo "Hello";
+
+$a=array(1,2,3,4,5,6,9);
+print_r($a);
+echo "Hello";
+
+$a=array(1,2,3,4,5,6,9);
+print_r($a);
+echo "Hello";
+
+
+$a=array(1,2,3,4,5,6,9);
+print_r($a);
+echo "Hello";
 ?>
